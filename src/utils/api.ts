e3f506d196311a452a52a694b18f76ee39cf77c0
Oluwayimika <77396635+Excellence-arch@users.noreply.github.com>
2024-11-12 23:08:17 +0100

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `http://localhost:8080/api`
+const BASE_URL = `https://lampstand-ucc2.onrender.com/api`
 //  || 'https://yourapiurl.com'; // Replace with your actual base URL
 
 type RequestOptions = Omit<RequestInit, 'headers'> & {
