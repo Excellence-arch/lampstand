4b81deb7d057243a4080e7d62035a79812a1cdee
Oluwayimika <oladipupomichael9@gmail.com>
2024-11-13 22:24:13 +0100

    fix: api issue on prod

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}/api`;
 //  || 'https://yourapiurl.com'; // Replace with your actual base URL
 
 type RequestOptions = Omit<RequestInit, 'headers'> & {
